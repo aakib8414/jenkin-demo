c50abafb5217741adce531419b43899403056728
aakib <aakib@binmile.com>
2024-07-11 17:18:52 +0530

    context path

--- src/main/java/com/jen/controller/Login.java
+++ src/main/java/com/jen/controller/Login.java
@@ -2,6 +2,8 @@ package com.jen.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -11,5 +13,17 @@ public class Login {
     public String login() {
 //        System.out.println("inside login page");
         return "login";
+    }
+
+    @PostMapping("/submit")
+    public String submit(@RequestParam("username") String username,
+                         @RequestParam("password") String password) {
+//        System.out.println("inside login page");
+        if ("admin".equals(username) && "admin".equals(password)) {
+            return "success"; // Redirect to success page
+        } else {
+            return "redirect:/error"; // Redirect to error page
+        }
+//        return "login";
     }
 }
